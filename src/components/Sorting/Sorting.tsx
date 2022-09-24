6054a07c3188d6d06aa71b61dabdff8ba2f37418
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2022-09-24 09:23:35 +0200

    work on Sorting products: add FormControl and Select

--- src/components/Sorting/Sorting.tsx
+++ src/components/Sorting/Sorting.tsx
@@ -1,40 +1,36 @@
 import * as React from 'react';
 
 import { Select, MenuItem, SelectChangeEvent } from '@mui/material';
-import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 import { typesSort } from '../../goods';
 
+const {
+  DEFAULT,
+  BY_NAME,
+  PRICE_LOW_TO_HIGH,
+  PRICE_HIGH_TO_LOW,
+  NUMBER_OF_PRICES,
+  WEIGHT,
+} = typesSort;
+
 export const Sorting = () => {
-  const [age, setAge] = React.useState('');
+  const [age, setAge] = React.useState(DEFAULT);
+  console.log(age);
 
   const handleChange = (event: SelectChangeEvent) => {
     setAge(event.target.value);
   };
 
-  const {
-    DEFAULT,
-    BY_NAME,
-    PRICE_LOW_TO_HIGH,
-    PRICE_HIGH_TO_LOW,
-    NUMBER_OF_PRICES,
-    WEIGHT,
-  } = typesSort;
   return (
     <>
       <FormControl sx={{ m: 1, minWidth: 200 }}>
-        <Select
-          value={age}
-          autoWidth
-          onChange={handleChange}
-          // sx={{ backgroundColor: 'tomato' }}
-        >
-          <MenuItem value={10}>{DEFAULT}</MenuItem>
-          <MenuItem value={20}>{BY_NAME}</MenuItem>
-          <MenuItem value={30}>{PRICE_LOW_TO_HIGH}</MenuItem>
-          <MenuItem value={40}>{PRICE_HIGH_TO_LOW}</MenuItem>
-          <MenuItem value={50}>{NUMBER_OF_PRICES}</MenuItem>
-          <MenuItem value={60}>{WEIGHT}</MenuItem>
+        <Select value={age} autoWidth onChange={handleChange}>
+          <MenuItem value={DEFAULT}>{DEFAULT}</MenuItem>
+          <MenuItem value={BY_NAME}>{BY_NAME}</MenuItem>
+          <MenuItem value={PRICE_LOW_TO_HIGH}>{PRICE_LOW_TO_HIGH}</MenuItem>
+          <MenuItem value={PRICE_HIGH_TO_LOW}>{PRICE_HIGH_TO_LOW}</MenuItem>
+          <MenuItem value={NUMBER_OF_PRICES}>{NUMBER_OF_PRICES}</MenuItem>
+          <MenuItem value={WEIGHT}>{WEIGHT}</MenuItem>
         </Select>
       </FormControl>
     </>
