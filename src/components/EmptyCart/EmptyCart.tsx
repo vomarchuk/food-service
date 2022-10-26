2eb855b3528bee4f6f2142bfb72ccf7cefac3b60
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2022-10-26 21:35:25 +0200

    working on EmptyCart plus googleMaps

--- src/components/EmptyCart/EmptyCart.tsx
+++ src/components/EmptyCart/EmptyCart.tsx
@@ -1,4 +1,6 @@
 import { Box } from '@mui/material';
+import { GoogleMaps } from '../../components/GoogleMaps';
+
 import style from './EmptyCart.module.scss';
 
 export const EmptyCart = () => {
@@ -7,6 +9,13 @@ export const EmptyCart = () => {
       <h2 className={style.title}>Your shopping cart is empty.</h2>
       <p className={style.text}>Add something faster!</p>
       <p className={style['text-accent']}>Free delivery from 100 zl</p>
+      <Box sx={styles.forBox}>
+        <GoogleMaps />
+      </Box>
     </Box>
   );
+};
+
+const styles = {
+  forBox: { marginTop: '20px' },
 };
---
+++ src/components/GoogleMaps/GoogleMaps.tsx
@@ -0,0 +1,89 @@
+import { Container } from '../../components/Container';
+
+import { useRef, useState } from 'react';
+
+import { Box, Skeleton } from '@mui/material';
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Marker,
+  Autocomplete,
+  DirectionsRenderer,
+} from '@react-google-maps/api';
+
+export const GoogleMaps = () => {
+  const ourCoordinates = { lat: 52.252692, lng: 21.0336633 };
+
+  const [map, setMap] = useState(/** @type google.maps.Map */ null);
+  const [directionsResponse, setDirectionsResponse] = useState<any>(null);
+  const [distance, setDistance] = useState<any>(null);
+  const [duration, setDuration] = useState<any>(null);
+
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const destinationRef = useRef<any | undefined>();
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY || '',
+    libraries: ['places'],
+  });
+
+  if (!isLoaded) {
+    return <Skeleton />;
+  }
+
+  async function calculateRoute() {
+    const coordinates = '52.252692 21.0336633';
+
+    if (destinationRef.current.value === '') {
+      return;
+    }
+    const directionsService = new google.maps.DirectionsService();
+    const results = await directionsService.route({
+      origin: coordinates,
+      destination: destinationRef.current.value,
+      travelMode: google.maps.TravelMode.DRIVING,
+    });
+    setDistance(results.routes[0].legs[0].distance?.text);
+    setDuration(results.routes[0].legs[0].duration?.text);
+    setDirectionsResponse(results);
+  }
+
+  return (
+    <Box position="relative">
+      <Box
+        // position="absolute"
+        width="100%"
+        height="100%"
+        zIndex="modal"
+      >
+        {/* {Google Maps Box} */}
+        <GoogleMap
+          center={ourCoordinates}
+          zoom={15}
+          mapContainerStyle={{ width: '100%', height: '300px' }}
+          options={{
+            zoomControl: false,
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullscreenControl: false,
+          }}
+          onLoad={(map: any) => setMap(map)}
+        >
+          <Marker position={ourCoordinates} />
+
+          {directionsResponse && (
+            <DirectionsRenderer directions={directionsResponse} />
+          )}
+        </GoogleMap>
+      </Box>
+      <Box>
+        <Autocomplete>
+          <input type="text" placeholder="Destination" ref={destinationRef} />
+        </Autocomplete>
+        <button type="button" onClick={calculateRoute}>
+          Click
+        </button>
+      </Box>
+    </Box>
+  );
+};
