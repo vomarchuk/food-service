d4fcd0afcef3e339e08580df721c399aba6e9f64
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2022-12-28 21:25:35 +0100

    finished to work about cart state

---
+++ src/redux/reviews/reviewsActions.jsx
@@ -0,0 +1,8 @@
+// import { createAction } from '@reduxjs/toolkit';
+
+// //pending
+// export const fetchReviewsRequest = createAction('reviews/fetchReviewsRequest');
+// //fulfilled
+// export const fetchReviewsSuccess = createAction('reviews/fetchReviewsSuccess');
+// //rejected
+// export const fetchReviewsError = createAction('reviews/fetchReviewsError');
